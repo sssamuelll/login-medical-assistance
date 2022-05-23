0ead1becd789982269407e2417366f2dcbc3e8bb
sssamuelll <sssamuelll@gmail.com>
2022-05-23 17:25:03 -0400

    creación del app de login en React

---
+++ src/static/js/LoginForm.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+function LoginForm({ Login, error }) {
+    const [details, setDetails] =useState({username: "", password: ""});
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+        Login(details);
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+    
+            <div className="overlay__inner">
+
+                <h1 className="overlay__title">
+                    Login UX
+                </h1>
+                <p className="overlay__description">Please sign in</p>
+
+                {(error !="") ? ( <div className="error">{error}</div> ) : ""}
+
+                <div className="overlay__form">
+
+                <div className="form-group">
+                  <label for="username">Username</label>
+                  <input type="text" className="form-control" name="username" id="username" onChange={e => setDetails({...details, username: e.target.value})} value={details.username} />
+                </div>
+
+                <div className="form-group">
+                  <label for="password">Password</label>
+                  <input type="password" className="form-control" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password} />
+                </div>
+                <div className="checkbox mb-3">
+                  <label>
+                    <input type="checkbox" value="remember-me" />
+                  </label>
+                </div>
+
+              </div>
+
+
+              <div className="form-group overlay__btns">
+
+                <button type="submit" className="overlay__btn overlay__btn--transparent">
+                  
+                    Sign In
+                  
+                </button>
+
+              </div>
+
+            </div>
+    
+        </form>
+    )
+}
+
+export default LoginForm
